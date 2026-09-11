4d6f83c6863e046c53263c506ecd470d0e86d383
SikandarJODD <sikandar6174@gmail.com>
2026-09-12 00:45:26 +0530

    feat: update Hero component with dynamic favicon and improved layout

--- src/lib/components/landing/hero.svelte
+++ src/lib/components/landing/hero.svelte
@@ -1,18 +1,28 @@
 <script lang="ts">
-	import { ArrowRight } from "@lucide/svelte";
+	import FaviconLink from "$lib/components/docs/base/main/favicon-link.svelte";
 	import { Button } from "$lib/components/ui/button";
+	import { components } from "$lib/registry/components";
+	import { Annotation } from "$ui/annotation";
+	import { mode } from "mode-watcher";
+	let faviconUrl = $derived.by(() => {
+		if (mode.current === "dark") {
+			return "https://assets.chanhdai.com/images/favicon-dark.svg";
+		} else {
+			return "https://assets.chanhdai.com/images/favicon.ico";
+		}
+	});
 </script>
 
-<section class="mx-auto w-full max-w-6xl overflow-hidden pt-16">
+<section class="mx-auto w-full max-w-6xl overflow-hidden py-20 sm:py-24">
 	<div aria-hidden="true" class="absolute inset-0 size-full overflow-hidden">
 		<div
 			class="absolute inset-0 isolate -z-10 bg-[radial-gradient(20%_80%_at_20%_0%,color-mix(in_oklab,var(--foreground)_10%,transparent),transparent)]"
 		></div>
 	</div>
 
-	<div class="relative z-10 flex max-w-2xl flex-col gap-5 px-4">
-		<a
-			class="group flex w-fit animate-in items-center gap-3 rounded-full border bg-card p-1 shadow-xs transition-all delay-500 duration-500 ease-out fill-mode-backwards slide-in-from-bottom-10 fade-in"
+	<div class="relative z-10 flex max-w-3xl flex-col px-4">
+		<!-- <a
+			class="group flex w-fit animate-in items-center gap-3 rounded-full border bg-card p-1 shadow-xs transition-all delay-50 duration-500 ease-out fill-mode-backwards slide-in-from-bottom-10 fade-in"
 			href="/"
 		>
 			<div class="rounded-full border bg-card px-2 py-0.5 shadow-sm">
@@ -27,56 +37,53 @@
 					class="size-3 -translate-x-0.5 duration-150 ease-out group-hover:translate-x-0.5"
 				/>
 			</div>
-		</a>
+		</a> -->
 
 		<h1
 			class="animate-in text-4xl leading-tight font-medium text-balance text-foreground delay-100 duration-500 ease-out fill-mode-backwards slide-in-from-bottom-10 fade-in md:text-5xl"
 		>
-			Svelte Project Setup
+			Svelte Chan Components
 		</h1>
 
 		<p
-			class="animate-in text-sm tracking-wider text-muted-foreground delay-200 duration-500 ease-out fill-mode-backwards slide-in-from-bottom-10 fade-in sm:text-lg md:text-xl"
+			class="mt-5 max-w-3xl animate-in font-mono text-sm lg:text-base leading-8 font-medium text-muted-foreground delay-200 duration-500 ease-out fill-mode-backwards slide-in-from-bottom-10 fade-in"
 		>
-			Build using Svelte, Tailwind CSS, and shadcn-svelte.
+			{components.length} Open-Source Portfolio components by
+			<Annotation
+				note="Original Creator"
+				direction="nw"
+				customColor="lab(36 55.64 -107.68)"
+				noMark
+			>
+				<FaviconLink
+					href="https://chanhdai.com"
+					target="_blank"
+					rel="noreferrer"
+					class="mx-0"
+					faviconSrc={faviconUrl}
+				>
+					Chánh Đại
+				</FaviconLink>
+			</Annotation>
+			<br />
+			Ported to Svelte by
+			<FaviconLink
+				href="https://bhide.dev"
+				target="_blank"
+				rel="noreferrer"
+				class=" text-orange-500 dark:text-orange-500 hover:text-orange-500/80"
+				imageClass="rounded-full size-4.5"
+			>
+				Bhide Svelte
+			</FaviconLink>
 		</p>
 
 		<div
-			class="flex w-fit animate-in items-center justify-center gap-3 pt-2 delay-300 duration-500 ease-out fill-mode-backwards slide-in-from-bottom-10 fade-in"
+			class="mt-10 flex w-fit animate-in items-center justify-center gap-3 delay-300 duration-500 ease-out fill-mode-backwards slide-in-from-bottom-10 fade-in"
 		>
-			<Button href="/docs">
-				Get Started
-				<ArrowRight class="size-4" />
-			</Button>
-			<Button href="/components/buttons" variant="outline">View Components</Button>
-		</div>
-	</div>
-
-	<div class="relative">
-		<div
-			class="absolute -inset-x-20 inset-y-0 -translate-y-1/3 scale-120 rounded-full bg-[radial-gradient(ellipse_at_center,color-mix(in_oklab,var(--foreground)_10%,transparent),transparent,transparent)] blur-[50px]"
-		></div>
-		<div
-			class="relative mt-8 -mr-56 animate-in overflow-hidden [mask-image:linear-gradient(to_bottom,black_0%,black_60%,transparent_100%)] px-2 delay-100 duration-1000 ease-out fill-mode-backwards slide-in-from-bottom-5 fade-in sm:mt-12 sm:mr-0 md:mt-20"
-		>
-			<div
-				class="relative mx-auto max-w-6xl overflow-hidden rounded-lg border bg-background p-2 shadow-xl ring-1 inset-shadow-2xs ring-card inset-shadow-foreground/10 dark:inset-shadow-xs dark:inset-shadow-foreground/20"
+			<Button href="/components/buttons" variant="ink"
+				>View Components</Button
 			>
-				<img
-					alt="app screen"
-					class="z-2 aspect-video rounded-lg border dark:hidden"
-					height="1080"
-					src="https://storage.efferd.com/screen/dashboard-light.webp"
-					width="1920"
-				/>
-				<img
-					alt="app screen"
-					class="hidden aspect-video rounded-lg bg-background dark:block"
-					height="1080"
-					src="https://storage.efferd.com/screen/dashboard-dark.webp"
-					width="1920"
-				/>
-			</div>
 		</div>
 	</div>
 </section>
